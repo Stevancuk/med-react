8b65e876923f379eb10c18fbe781d7214590252d
Stevancuk <stevancuk@yahoo.com>
2021-10-19 20:09:50 +0200

    remove app.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import TopMenu from './components/TopMenu/TopMenu';
 import NoviKlijent from './components/NoviKlijent/NoviKlijent';
 import ListaKlijenata from './components/ListaKlijenata/ListaKlijenata';
 import Zakazivanje from './components/Zakazivanje/Zakazivanje';
-<<<<<<< Updated upstream
 import KlijentProfile from './components/KlijentProfile/KlijentProfile';
-
-=======
-import Navbar from './components/Navbar/Navbar';
->>>>>>> Stashed changes
+// import Navbar from './components/Navbar/Navbar';
 import NoviTretman from './components/NoviTretman/NoviTretman';
 import { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
